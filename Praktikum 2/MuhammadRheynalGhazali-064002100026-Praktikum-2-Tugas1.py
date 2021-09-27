1948d0f3ccfce50fe6c69ffa4339df84821239e0
Rheynal <91449041+Rheynal@users.noreply.github.com>
2021-09-27 14:04:31 +0700

    Upload File Praktikum 2 Tugas 1

---
+++ Praktikum 2/MuhammadRheynalGhazali-064002100026-Praktikum-2-Tugas1.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 27 13:51:24 2021
+
+
+@author: mrhey
+NAMA : Muhammad Rheynal Ghazali
+NIM : 064002100026
+"""
+
+
+import math
+print("tugas praktikum 2 algoritma dasar")
+
+a = float(input("masukan nilai a = "))
+b = float(input("masukan nilai b = "))
+
+
+hasil  = a + b
+print("hasil a tambah b adalah",hasil)
+#################################
+hasil = a - b
+print("hasil a kurang b adalah",abs(hasil))
+#################################
+hasil = a * b
+print("hasil a dikali b adalah",hasil)
+#################################
+hasil = a / b
+print("hasil a dibagi b adalah",hasil)
+#################################
+hasil = a % b
+print("hasil sisa pembagian a dan b adalah",hasil)
+#################################
+hasil = math.log10(a)
+print("hasil log a dan b adalah",hasil)
+#################################
+hasil = a ** b
+print("hasil a pangkat b adalah",hasil)
